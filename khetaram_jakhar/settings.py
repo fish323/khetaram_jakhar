99807b54ed13b150b1742a91fa788d996e61bdc9
fish323 <rahuljakhar323@gmail.com>
2025-12-18 01:08:36 +0530

    Update settings.py

--- khetaram_jakhar/settings.py
+++ khetaram_jakhar/settings.py
@@ -164,7 +164,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 # --- TWILIO WHATSAPP CONFIGURATION ---
 # Replace these with your actual keys from Twilio Console
 TWILIO_ACCOUNT_SID = 'AC83a3beb24a30fb8c0cde99ce557ca8c3'  # Your Account SID
-TWILIO_AUTH_TOKEN = '75f37c55a95c0e2331c9f74094aade4b'    # Your Auth Token
+TWILIO_AUTH_TOKEN = 'a3d73bbe4c84557ed84adf43d1075358'    # Your Auth Token
 # The Sandbox number (usually whatsapp:+14155238886)
 TWILIO_VERIFY_SERVICE_SID = 'VAf3718ecaa309d98f3d6fe0207e62c65a'
 TWILIO_SANDBOX_NUMBER = 'whatsapp:+14155238886'
